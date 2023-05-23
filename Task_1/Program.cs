abdb13064aa28044281ea37f8825ea6f1cf7b4e4
Aleksandr <abreskolenko@yandex.ru>
2023-05-23 19:46:16 +0300

    Task 1 completed

--- Task_1/Program.cs
+++ Task_1/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.IO;
+
+class Program
+{
+    //Метод создания каталога и удаление его по времени
+    public static void CreateFolder()
+    {
+        DirectoryInfo createFolder = new DirectoryInfo("C:\\Users\\abres\\Desktop\\NewFolder");
+        
+        //Если папки не существует то создаем ее
+        if(!createFolder.Exists)
+        {
+            createFolder.Create();
+            Console.WriteLine("Папка NewFolder создана.");
+        }
+
+        TimeSpan ts30 = TimeSpan.FromMinutes(30);
+
+        //Время создания папки
+        var startTime = createFolder.CreationTime;
+        //Текущее время
+        var endTime = DateTime.Now;
+
+        var result = endTime - startTime;
+        Console.WriteLine(result);
+
+        if (result > ts30)
+        {
+            createFolder.Delete(true);
+            Console.WriteLine("Директория удалена.");
+        }
+
+    }
+
+    static void Main(string[] args)
+    {
+        CreateFolder();
+    }
+}
